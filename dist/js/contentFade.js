9347c162a30a1a6577da690e0f6991e1bf3c1247
NiloyGhosh <55622147+niloyKGhosh@users.noreply.github.com>
2019-12-15 23:17:25 -0500

    improved the animation for fading

--- dist/js/contentFade.js
+++ dist/js/contentFade.js
@@ -2,7 +2,7 @@ function contentFadeIn(text) {
     var content = document.querySelector(text);
     var contentPosition = content.getBoundingClientRect().top;
     console.log(contentPosition);
-    var screenPosition = window.innerHeight;
+    var screenPosition = window.innerHeight / 2;
 
     if (contentPosition < screenPosition) {
         content.classList.add('fade-in');
