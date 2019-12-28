ce4962844989240501e79427d23ad1b8989e8de9
NiloyGhosh <55622147+niloyKGhosh@users.noreply.github.com>
2019-12-28 02:03:49 -0500

    Created sticky using jQuery. Fixed some CSS to eliminate the bouncing effect when padding changes in the navigation bar

--- dist/js/stickyNav.js
+++ dist/js/stickyNav.js
@@ -1,20 +1,12 @@
-function stickyNavigation () {
-    const about = document.querySelector('.about-fade');
-    const navBar = document.querySelector('.ul-nav');
-    const stickyNavPos = about.offsetTop - 100; // Subtracted 100 for appearing early
-
-    if (window.scrollY >= stickyNavPos) {
-        navBar.classList.add('sticky-nav');
-        navBar.style.padding = '1rem';
-    } else {
-        navBar.classList.remove('sticky-nav');
-        navBar.style.paddingTop = '3rem';
-        navBar.style.paddingBottom = '3rem';
-        navBar.style.paddingRight = '0';
-        navBar.style.paddingLeft = '0';
-
-    }
-
-}
-
-window.addEventListener('scroll', stickyNavigation);
+$(document).ready(function() {
+    let h = $(".about-fade").offset().top - 100;
+    $(window).on('scroll', function() {
+        let scrollPosition = $(window).scrollTop();
+        if (scrollPosition >= h) {
+            $(".ul-nav").addClass("sticky-nav");
+        }
+        else {
+            $(".ul-nav").removeClass("sticky-nav");
+        }
+    });
+});
