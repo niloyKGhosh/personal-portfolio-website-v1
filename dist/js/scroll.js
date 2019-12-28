e735366f93e2e5def3a9cb38722d6d3f5f071d85
NiloyGhosh <55622147+niloyKGhosh@users.noreply.github.com>
2019-12-27 23:48:40 -0500

    Removed Code Repetition by creating a separate function to handle events

--- dist/js/scroll.js
+++ dist/js/scroll.js
@@ -1,25 +1,17 @@
 // Smooth Scroll using jQuery
 $(document).ready(function() {
-    $('.ul-nav a').on('click', function(event) {
-        if (this.hash != '') {
-            event.preventDefault();
-            const hash = this.hash;
+    $('.ul-nav a').on('click', smoothScroll);
     
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 900);
-        }
-    });
-    
-    $('#more-about-me').on('click', function(event) {
-        if (this.hash != '') {
-            event.preventDefault();
-            const hash = this.hash;
-    
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 900);
-        }
-    });
+    $('#more-about-me').on('click', smoothScroll);
 });
 
+function smoothScroll(event) {
+    if (this.hash != '') {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900);
+    }
+}
